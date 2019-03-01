d5ac2fcb1c96f680386ea603618a1aed6e03f0f5
abluewish <35025796+abluewish@users.noreply.github.com>
2019-03-01 14:16:18 -0500

    Update Convert.cpp

--- Convert.cpp
+++ Convert.cpp
@@ -6,3 +6,19 @@ Morse::Morse(int pin)
   pinMode(pin, OUTPUT);
   _pin = pin;
 }
+
+void Morse::blink()
+{
+  digitalWrite(_pin, HIGH);
+  delay(250);
+  digitalWrite(_pin, LOW);
+  delay(250);  
+}
+
+void Morse::func()
+{
+  digitalWrite(_pin, HIGH);
+  delay(1000);
+  digitalWrite(_pin, LOW);
+  delay(250);
+}

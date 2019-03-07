cadfd5f9e3c7d068f172b173de8a224305f01414
abluewish <35025796+abluewish@users.noreply.github.com>
2019-03-07 12:07:15 -0500

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,13 +1,32 @@
-#include <Convert.h>
+#include<Arduino.h>
+#include<Convert.h>
 
-Convert convert(13);
+int sensorValue = 0;
+int ledPin = 13;
 
-void setup()
-{
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPin,OUTPUT);
 }
 
-void loop()
-{
-  Convert.blink();
-  delay(3000);
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue=analogRead(A0);
+  float voltage = sensorValue*(5.0/1023.0);
+  Serial.println(sensorValue);
+  int blightness=voltage/2;
+  delay(2);
+  if(sensorValue>300){
+    int t1=1;
+    blink(t1);
+    delay(2);
+  }
+}
+
+void blink(int t){
+  digitalWrite(ledPin,HIGH);
+  delay(t);
+  digitalWrite(ledPin,LOW);
+  delay(t);
 }

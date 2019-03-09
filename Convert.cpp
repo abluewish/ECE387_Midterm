001094e8cbf215cd82e32c2e4593f6c9126ca543
abluewish <35025796+abluewish@users.noreply.github.com>
2019-03-09 00:20:03 -0500

    Update Convert.cpp

--- Convert.cpp
+++ Convert.cpp
@@ -1,21 +1,51 @@
 #include "Arduino.h"
 #include "Convert.h"
 
+
 Convert::Convert(int pin)
 {
   pinMode(pin, OUTPUT);
   _pin = pin;
 }
 
-void Convert::blink(int t)  // let LED blink in a period of 2t
+void Convert::blink(int t)
 {
   digitalWrite(_pin, HIGH);
-  delay(t);
+  delay(250);
   digitalWrite(_pin, LOW);
-  delay(t);  
+  delay(250);  
 }
 
-float Convert::func(float v) //convert the voltage signal from sensor to the PWM input we will use
+void Convert::func(float v)
 {
-    return 300/v*255;
+    if(v < 0)
+        break;
+    int b = v/5*225;
+    analogWrite(_pin, b);
+    delay(10);
+}
+
+float Convert::base(float b[]){
+    bool flag=true;
+    float mean=b[0];
+    for(int i=0;i<sizeof(b)-1;i++){
+        if(abs(b[1]-b[0])>0.2 && abs(b[2]-b[0])>abs(b[2]-b[1]) && i==0){
+            mean=(b[1]+b[2])/2;
+            i++;
+        }
+        if (abs(b[i]-mean)>0.2) //elimate undesired data
+            flag=false;
+        if(flag){
+            mean+=b[i];
+            mean/=2;
+        }
+    }
+    return mean;
+}
+
+bool Convert::light(int count, float v, float c){
+    if(count>20 && v>0.5)
+        if(abs(c-v)<=0.2*v)
+            return true;
+    return false;
 }
